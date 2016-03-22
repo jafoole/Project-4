9037622298c12e8b8d84faffcdfa4b1d17243dcb
jafoole <smallbabba@gmail.com>
2016-03-22 17:34:54 -0400

    Second Commit - Layouts

--- Zaba/app/src/main/java/com/example/jafoole/zaba/BookFlightActivity.java
+++ Zaba/app/src/main/java/com/example/jafoole/zaba/BookFlightActivity.java
@@ -1,13 +1,30 @@
 package com.example.jafoole.zaba;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
 
 public class BookFlightActivity extends AppCompatActivity {
+
+    ImageView backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_flight);
+
+
+        backButton = (ImageView)findViewById(R.id.backButton2);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent backIntent = new Intent(BookFlightActivity.this, FlightsListActivity.class);
+                startActivity(backIntent);
+            }
+        });
     }
 }
--- Zaba/app/src/main/java/com/example/jafoole/zaba/FlightsListActivity.java
+++ Zaba/app/src/main/java/com/example/jafoole/zaba/FlightsListActivity.java
@@ -16,10 +16,7 @@ public class FlightsListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FlightsListActivity.this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_flights_list);
-        Toolbar toolbar = (Toolbar)findViewById(R.id.my_toolbar);
-        setSupportActionBar(toolbar);
 
         backButton = (ImageView)findViewById(R.id.backButton1);
 
